98ab533a31df7881790871a4dbbe1a5c686c9b52
john-connor98 <68577331+john-connor98@users.noreply.github.com>
2020-07-22 16:46:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from sklearn.metrics import pairwise_distances
 from flask_cors import cross_origin
 from flask_sqlalchemy import SQLAlchemy
 import numpy as np
-import numpy
+# import numpy
 import pandas as pd
 import re
 import json
@@ -25,8 +25,8 @@ def addapt_numpy_float64(numpy_float64):
     return AsIs(numpy_float64)
 def addapt_numpy_int64(numpy_int64):
     return AsIs(numpy_int64)
-register_adapter(numpy.float64, addapt_numpy_float64)
-register_adapter(numpy.int64, addapt_numpy_int64)
+register_adapter(np.float64, addapt_numpy_float64)
+register_adapter(np.int64, addapt_numpy_int64)
 
 class studdata(db.Model):
     __tablename__ = 'studydata'
