efdd10b5c9e4f53fb2583b05053a80d43431a234
john-connor98 <68577331+john-connor98@users.noreply.github.com>
2020-07-22 18:22:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def manage_query(req):
     if index==None:
         ans = "sorry check the database "
     else:
-        ans = str(db.session.query(studdata).get(index+1).all())
+        ans = str(studdata.query(studdata).get(index+1))
     if ans == "None":
         ans = "there is some problem in answer"
 
